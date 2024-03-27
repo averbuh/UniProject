3b7c79c3829fd06a49e5dd1b40decad5c0287032
Alex <ebta.alex.russia@gmail.com>
2024-03-27 18:39:08 +0200

    add terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
