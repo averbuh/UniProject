a48a681f89d372d590e697d8d63fa2eab1af35f4
averbuh <averbuh.alex.in@gmail.com>
2024-06-26 02:45:51 +0300

    Refactor branch filtering regex to use forward slashes for workflow branches. Update paths in CI config. Add favorites JSON data. Create values file for frontend in Helm chart.
    The commit refactors branch filtering regex in the CI workflow config, updates paths, adds favorites JSON data, and creates a dedicated values file for the frontend in the Helm chart. No specific issues addressed.

--- terraform/terraform-eks-argocd/eks.tf
+++ terraform/terraform-eks-argocd/eks.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster" {
-  name               = "eks-cluster"
+  name               = "eks-cluster-argo"
   assume_role_policy = data.aws_iam_policy_document.assume_role_eks.json
 }
 
