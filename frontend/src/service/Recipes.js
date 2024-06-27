3df9d0b1b9d538d02c8c8de1829c08510a2a1e46
averbuh <averbuh.alex.in@gmail.com>
2024-06-28 02:17:28 +0300

    Changes urls

--- frontend/src/service/Recipes.js
+++ frontend/src/service/Recipes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = window.location.origin + '/api'
+const url = 'http://afd1df950f69b4a29b47d82a5a1cd9f4-856604395.eu-central-1.elb.amazonaws.com'
 // const url_dev = 'http://localhost:8080'
 let response
 export const Recipes = {
--- frontend/src/service/Restaurant.js
+++ frontend/src/service/Restaurant.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8080'
+const url = 'http://afd1df950f69b4a29b47d82a5a1cd9f4-856604395.eu-central-1.elb.amazonaws.com'
+// const url = 'http://localhost:8080'
 
 export const Restaurant = {
   async getRestaurantsData() {
