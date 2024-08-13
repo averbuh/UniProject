760bbdcd01f87dfd82cfa81019b78d18b54813e3
averbuh <averbuh.alex.in@gmail.com>
2024-08-13 21:03:08 +0300

    new script

---
+++ release-branch.sh
@@ -0,0 +1,7 @@
+# Shell script to create a pre-release branch
+git checkout main
+git pull origin main
+latest_tag=$(npx semantic-release --dry-run | grep "The next release version is" | awk '{print $6}')
+pre_release_branch="prerelease/v${latest_tag}-rc.1"
+git checkout -b $pre_release_branch
+git push origin $pre_release_branch
