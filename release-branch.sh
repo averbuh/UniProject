8c7bc64cef53d135102caf4fd40a2926207db405
averbuh <averbuh.alex.in@gmail.com>
2024-08-13 21:03:08 +0300

    new script

--- release-branch.sh
+++ release-branch.sh
@@ -4,7 +4,3 @@ git pull origin main
 latest_tag=$(npx semantic-release --dry-run | grep "The next release version is" | awk '{print $6}')
 pre_release_branch="prerelease/v${latest_tag}-rc.1"
 git checkout -b $pre_release_branch
-git push origin $pre_release_branch
-
-
-git tag -l 'v?.?.?' --sort=creatordate | tail -n 1
